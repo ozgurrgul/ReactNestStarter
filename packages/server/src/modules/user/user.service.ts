2eb59f6d490f8aefbccff0ee9e2ea387f3fffcdc
Ozgur <lugrugzo@gmail.com>
2022-08-21 14:32:42 +0200

    Remove unused block

--- packages/server/src/modules/user/user.service.ts
+++ packages/server/src/modules/user/user.service.ts
@@ -15,23 +15,6 @@ export class UserService {
     });
   }
 
-  async users(params: {
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-  }): Promise<User[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.user.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
-  }
-
   async createUser({ email, password, fullName }: RegisterInput) {
     try {
       await this.prisma.user.create({
@@ -45,22 +28,5 @@ export class UserService {
       }
       throw e;
     }
-  }
-
-  async updateUser(params: {
-    where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
-  }): Promise<User> {
-    const { where, data } = params;
-    return this.prisma.user.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({
-      where,
-    });
   }
 }
